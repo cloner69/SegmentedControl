1a4776d06085a345000adefb64b0544146fe508c
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 02:54:24 +0700

    segmented control

---
+++ SegmentedControlAnimation/SegmentedControlAnimation/Model/Tab.swift
@@ -0,0 +1,14 @@
+//
+//  Tab.swift
+//  SegmentedControlAnimation
+//
+//  Created by Adrian Suryo Abiyoga on 10/02/25.
+//
+
+import SwiftUI
+
+/// - Enum Tab Cases
+enum Tab: String{
+    case photo = "Photo"
+    case video = "Video"
+}
---
+++ SegmentedControlAnimation/SegmentedControlAnimation/SegmentedControlAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlAnimationApp.swift
+//  SegmentedControlAnimation
+//
+//  Created by Adrian Suryo Abiyoga on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
